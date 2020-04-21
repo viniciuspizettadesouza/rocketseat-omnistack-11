4e1bee5b9ec9ea5362bf4b06263b8ce9236267df
viniciuspzt <viniciuspzt@gmail.com>
2020-04-21 19:39:35 -0300

    first commit

---
+++ backend/index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', (request, response) => {
+    return response.json({
+        evento: 'Semana Omnistack 11.0',
+        aluno: 'Pizetta'
+    })
+})
+
+app.listen(3333)
